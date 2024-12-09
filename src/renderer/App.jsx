8f9a04ee84b4ddbaec48256d50dc9cf2f7c87a1d
Felipe S. Oliver <felipesantosdd@gmail.com>
2024-12-09 20:12:00 -0300

    feat/adição do mui

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -1,9 +1,11 @@
+import { Button } from '@mui/material';
 import React from 'react';
 
 const App = () => {
     return (
         <div>
             <h1>parece funcionar, mas com um certo dalay</h1>
+            <Button>Um Botão do Mui</Button>
         </div>
     );
 };
